a5fede36a88e0c38c28617c82994a1ed32967258
Maulik Khandwala <maulikk2000@gmail.com>
2020-02-19 22:40:30 +1100

    Updates

---
+++ precache-manifest.1634b09dfb9d4bf7e247c73ace7a7e3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bfe175f169a49f476cab17a9a0f4dc2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8a50f34827c5b4446e3c",
+    "url": "/monsters-rolodex/static/css/main.fc53bd1e.chunk.css"
+  },
+  {
+    "revision": "5839ed3c83c8fcce4b8e",
+    "url": "/monsters-rolodex/static/js/2.7b328a78.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.7b328a78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a50f34827c5b4446e3c",
+    "url": "/monsters-rolodex/static/js/main.42b5dd47.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
